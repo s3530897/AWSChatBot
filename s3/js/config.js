7f70652d4bd2d994c7bbfafba8b3ab2769562e8e
s3530897 <30851128+s3530897@users.noreply.github.com>
2018-03-26 11:03:32 +1100

    V2
    V2

---
+++ s3/js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-1_eF6Ucab0s', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '5fu4tdauu4m88q2c31sd02plqh', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: 'Invoke URL: https://rwln0huvgb.execute-api.us-east-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
