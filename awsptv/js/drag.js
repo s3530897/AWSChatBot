c0af65bdf9758554e42baf93823d8c4533cd55a2
s3530897 <30851128+s3530897@users.noreply.github.com>
2018-04-08 14:28:01 +1000

    PTV

--- awsptv/js/drag.js
+++ awsptv/js/drag.js
@@ -8,27 +8,28 @@ $.fn.extend({
     	$("#small_button").click( function () {
 				var w=$(window).width()-$(".box-3 dl").width();
 				var h=$(window).height()-$(".box-3 dl dd").height();
-				var h1=$(window).height()-475;			
+				var h1=$(window).height()-475;				
 			if ( $(this).hasClass("up") ){
 					$(this).removeClass("up").addClass("down");
-					$(".box-3 dl").find("ul").hide();	
+					$(".box-3 dl").find("ul").show();
 					//$(".box-3 dl").animate({
 					//	left:w+"px",
 					//	top:h+"px"
-					//},200);														
-				}else{
+					//},200);
+					$("#botname").text("● PTV Bot");								
+				}else{	
 					$(this).removeClass("down").addClass("up");
-					$(".box-3 dl").find("ul").show();
+					$(".box-3 dl").find("ul").hide();	
 					//$(".box-3 dl").animate({
 					//	left:(w-80)+"px",
 					//	top:h1+"px"
-					//},200);						
+					//},200);
+					$("#botname").text("● Need help? Click me!");					
 				}
 		
 		 });	
 		 $("#small_button2").click( function () {
-			 $("#chatform").submit();
-				//$("#small_button").click();
+			$("#small_button").click();
 		 });			
     }
 	
